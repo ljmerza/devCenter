a8a74bf93a634d0984a183bd8576e27f019d883b
ljmerza <ljmerza@gmail.com>
2018-02-12 08:36:40 -0500

    sql flask routes

---
+++ server/Flask/Requests/UserRequests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from Flask import FlaskUtils
+
+def get_navbar_items(data, sql_obj):
+	'''
+	'''
+
+	session = sql_obj.login()
+	data = sql_obj.get_navbar_items(session)
+	sql_obj.logout(session)
+
+	return {
+		'status': True,
+		'data': data
+	}
---
+++ server/Flask/Routes/UserRoutes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from flask import Response
+from flask_cors import cross_origin
+
+import Requests.UserRequests as UserRequests
+import Requests.JiraRequests as JiraRequests
+
+def define_routes(app, app_name, jira_obj, sql_obj, g):
+
+	@app.route(f'/{app_name}/jira/profile/<username>')
+	@cross_origin()
+	def get_profile(username):
+		'''
+		'''
+		data = {
+			"cred_hash": g.cred_hash,
+			"username": username
+		}
+		data = JiraRequests.get_profile(data=data, jira_obj=jira_obj, sql_obj=sql_obj)
+		return Response(data, mimetype='application/json')
+
+	@app.route(f'/{app_name}/navbar')
+	@cross_origin()
+	def get_navbar(username):
+		'''
+		'''
+		data = UserRequests.get_navbar_items(sql_obj=sql_obj)
+		return Response(data, mimetype='application/json')
