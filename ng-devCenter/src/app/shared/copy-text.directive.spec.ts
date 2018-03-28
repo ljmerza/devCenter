5216dedbb8475f1b9b172088240055b25a74464a
ljmerza <ljmerza@gmail.com>
2018-03-28 16:03:50 -0400

    automated commit

--- ng-devCenter/src/app/shared/copy-text.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CopyTextDirective } from './copy-text.directive';
-
-describe('CopyTextDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CopyTextDirective();
-    expect(directive).toBeTruthy();
-  });
-});
