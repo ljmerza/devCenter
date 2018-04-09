4472e6666f42ddb8b713ae15a811339e224b2ae5
ljmerza <ljmerza@gmail.com>
2018-04-09 15:28:20 -0400

    automated commit

---
+++ ng-devCenter/version.sh
@@ -0,0 +1,6 @@
+version_file=src/app/app.version.ts
+value=`cat package.json`
+echo "$value";
+echo "// This file was generated on $(date)
+export const appVersion = $value;" > $version_file
+git add $version_file
