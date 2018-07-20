4a079a1469484736c964f3922116ab20977d72c3
ljmerza <ljmerza@gmail.com>
2018-07-20 16:37:06 -0400

    automated commit 20/07/2018 16:37:06

--- server/CodeCloud/Reviewers.py
+++ server/CodeCloud/Reviewers.py
@@ -5,8 +5,8 @@ class Reviewers():
 		self.code_cloud_api = code_cloud_api
 
 	def add_comment_to_pull_request(self, repo_name, pull_request_id, comment, cred_hash):
-		url = f'{self.code_cloud_api.code_cloud_branches_api}/{repo_name}/pull-requests/{pull_request_id}/comments'
-		return self.code_cloud_api.post(url=url, post_data={'text':comment}, cred_hash=cred_hash)
+		url = f'{self.code_cloud_api.code_cloud_branches_api}/{repo_name}/pull-requests/{pull_request_id}/comments.json'
+		return self.code_cloud_api.post(url=url, json_data={'text':comment}, cred_hash=cred_hash)
 
 	def add_reviewer_to_pull_request(self, username, repo_name, pull_request_id, cred_hash):
 		return self._change_pull_quest_status(username, repo_name, pull_request_id, cred_hash, role='REVIEWER')
@@ -16,7 +16,7 @@ class Reviewers():
 
 
 	def _change_pull_quest_status(self, username, repo_name, pull_request_id, cred_hash, role):
-		url = f'{self.code_cloud_api.code_cloud_branches_api}/{repo_name}/pull-requests/{pull_request_id}/participants/{username}'
+		url = f'{self.code_cloud_api.code_cloud_branches_api}/{repo_name}/pull-requests/{pull_request_id}/participants/{username}.json'
 		post_data = {'role':role}
 
-		return self.code_cloud_api.post(url=url, post_data=post_data, cred_hash=cred_hash)
+		return self.code_cloud_api.post(url=url, json_data=post_data, cred_hash=cred_hash)
